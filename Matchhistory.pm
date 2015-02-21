ee0d4edda20dae7d69359daa345816a99e027ad3
aptmac <aptmac@yorku.ca>
2015-02-20 21:56:23 -0500

    Update Matchhistory.pm

--- Matchhistory.pm
+++ Matchhistory.pm
@@ -21,7 +21,7 @@ my $baseURL = "https://na.api.pvp.net/api/lol";
 #Set the Region of interest
 my $region = "na";
 #Set the API Key
-my $key = "8cdd8483-d12f-4c16-a623-d603191c3c2f";
+my $key = "YOUR_API_KEY_HERE";
 
 #VARIABLE RETURN OVERVIEW
 #my $matchStats = {'totalGamesPlayed'    => $numGames,
